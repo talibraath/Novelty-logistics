94920ccf83555e86243e23174499e05b461980e6
Talib Raath <talib.raath@gmail.com>
2024-09-02 02:52:27 -0700

    Makeing chanegs

--- app/components/Team.tsx
+++ app/components/Team.tsx
@@ -7,7 +7,7 @@ export default function Team() {
           <h2 className="text-4xl font-bold mb-12">Meet Our Guide</h2>
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
             <div className="bg-white p-8 shadow-lg rounded-lg">
-              <Image src="https://randomuser.me/api/portraits/men/32.jpg" alt="Jordan" className="w-full h-64 object-cover rounded-t-lg" />
+
               <div className="mt-4">
                 <div className="flex justify-center space-x-4 mb-4">
                   <a href="#" className="text-green-500"><i className="fab fa-facebook"></i></a>
@@ -19,7 +19,6 @@ export default function Team() {
               </div>
             </div>
             <div className="bg-white p-8 shadow-lg rounded-lg">
-              <Image src="https://randomuser.me/api/portraits/women/44.jpg" alt="Annie" className="w-full h-64 object-cover rounded-t-lg" />
               <div className="mt-4">
                 <div className="flex justify-center space-x-4 mb-4">
                   <a href="#" className="text-green-500"><i className="fab fa-facebook"></i></a>
@@ -31,7 +30,6 @@ export default function Team() {
               </div>
             </div>
             <div className="bg-white p-8 shadow-lg rounded-lg">
-              <Image src="https://randomuser.me/api/portraits/men/45.jpg" alt="Ronnie" className="w-full h-64 object-cover rounded-t-lg" />
               <div className="mt-4">
                 <div className="flex justify-center space-x-4 mb-4">
                   <a href="#" className="text-green-500"><i className="fab fa-facebook"></i></a>
@@ -43,7 +41,6 @@ export default function Team() {
               </div>
             </div>
             <div className="bg-white p-8 shadow-lg rounded-lg">
-              <Image src="https://randomuser.me/api/portraits/women/56.jpg" alt="Marina" className="w-full h-64 object-cover rounded-t-lg" />
               <div className="mt-4">
                 <div className="flex justify-center space-x-4 mb-4">
                   <a href="#" className="text-green-500"><i className="fab fa-facebook"></i></a>
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    reactStrictMode: true,
+    images: {
+      domains: ['randomuser.me'], // Configure external domain here
+    },
+};
 
 export default nextConfig;
