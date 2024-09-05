98fffd760f2284f89192391daeedf8c2c3761ee4
Talib Raath <talib.raath@gmail.com>
2024-09-05 11:35:19 -0700

    web speed

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,6 @@
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from '@vercel/speed-insights/next';
+
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
@@ -23,6 +25,7 @@ export default function RootLayout({
       <body>
         {children}
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
